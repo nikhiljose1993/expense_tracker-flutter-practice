eeaaaaca8aecb91d584b13ead31a0769b1d69f22
nikhiljose18 <nikhiljose18@gmail.com>
2023-11-23 01:50:16 +0530

    Added Chart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat('dd/MM/yyyy');
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining_outlined,
+  Category.travel: Icons.flight_takeoff_outlined,
+  Category.leisure: Icons.movie_outlined,
+  Category.work: Icons.work_outline_outlined,
+};
 
 class Expense {
   Expense({
@@ -17,4 +28,33 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (var expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
 }
